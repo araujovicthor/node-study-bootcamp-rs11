00c5ec46377848d3c870c439e23cd6b2ddba769f
Victhor Araújo <victhor.araujo.41@gmail.com>
2020-08-18 22:10:08 -0300

    s3 storage provider

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -10,7 +10,7 @@ import UsersAvatarController from '../controllers/UsersAvatarController';
 
 const usersRouter = Router();
 
-const upload = multer(uploadConfig);
+const upload = multer(uploadConfig.multer);
 const usersController = new UsersController();
 const usersAvatarController = new UsersAvatarController();
 
